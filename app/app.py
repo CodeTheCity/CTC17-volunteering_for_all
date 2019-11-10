b125fbb6cf47ea10911d344e1926e307f1d280d7
Robert McWilliam <rmcw@allmail.net>
2019-11-10 11:50:44 +0000

    Start DB models.

--- app/app.py
+++ app/app.py
@@ -1,20 +1,12 @@
 
 from settings import *
 
-import sqlite3
+import os
+from flask_sqlalchemy import SQLAlchemy
 
-# Data storage helpers
-def get_db():
-    database = SQLITE_DB
-    db = sqlite3.connect(database)
-    db.row_factory = sqlite3.Row
-    return db
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "volunteer.db"))
 
-def init_db():
-    with open("schema.sql") as f:
-        db = get_db()
-        cur = db.cursor()
-        cur.executescript(f.read())
         
 # Web API
 
@@ -22,7 +14,67 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 app.debug = True
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
+db = SQLAlchemy(app)
+
+volunteer_skills = db.Table('volunteer_skills',
+    db.Column('volunteer_id', db.Integer, db.ForeignKey('volunteer.id'), primary_key=True),
+    db.Column('skill_id', db.Integer, db.ForeignKey('skill.id'), primary_key=True)
+)
+
+volunteer_interest = db.Table("volunteer_interest",
+    db.Column('volunteer_id', db.Integer, db.ForeignKey('volunteer.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+charity_category = db.Table('charity_category',
+    db.Column('charity_id', db.Integer, db.ForeignKey('charity.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+class Volunteer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    bio = db.Column(db.Text)
+    paused = db.Column(db.Boolean)
+    skills = db.relationship('Skill', secondary=volunteer_skills, lazy='subquery',
+                             backref=db.backref('skill', lazy=True))
+    interested = db.relationship('Interested', secondary=volunteer_interest, lazy='subquery',
+                                 backref=db.backref('category', lazy=True))
+    
+class Charity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.Text)
+    categories = db.relationship('Categories', secondary=charity_category, lazy='subquery',
+                                 backref=db.backref('category', lazy=True))
+    def __repr(self):
+        return "<Charity: {}>".format(self.name)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    def __repr__(self):
+        return "<Category: {}>".format(self.name)
+
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    def __repr__(self):
+        return "<Skill: {}>".format(self.name)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.Integer, db.ForeignKey("charity.id"), nullable=False)
+    description = db.Column(db.Text)
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.Integer, db.ForeignKey("task.id"), nullable=False)
+    volunteer = db.Column(db.Integer, db.ForeignKey("volunteer.id"), nullable=False)
+    
 from web import *
 
 if __name__ == "__main__":
